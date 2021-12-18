e9f08a9bb2732d3c75a9e14de7ac150b8313348d
amitzbb <amit-macbook@Amit-Macbooks-MacBook-Pro.local>
2021-12-18 18:28:09 +0200

    aaa

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app1 = Flask(__name__)
 def citiesTimeZones():
     return render_template("index.html")
 
-@app1.route('/favicon.ico') 
-def favicon(): 
-    return render_template("favicon.ico")
+
+
+app1.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='favicon.ico'))
 
 if __name__=='__main__':
     app1.run(debug=True,host='0.0.0.0',port=int(os.environ['port']))
